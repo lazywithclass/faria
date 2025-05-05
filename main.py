d7a24402d2d8229f277324b0ddafeff94525e6df
Alberto Zaccagni <montecristoh@gmail.com>
2025-05-05 12:28:45 +0200

    pass arguments to get either tui or extended summary

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from src.gemini_api import summarize_text, get_youtube_transcript
+from src.gemini_api import extended_summarize_text, get_youtube_transcript
 from src.logger import setup_logger
 from src.tui import VideoApp
 from src.youtube_auth import get_authenticated_service
 from src.youtube_user import get_subscription_feed
+import argparse
 
 
 def get_videos():
@@ -10,9 +11,9 @@ def get_videos():
     videos = get_subscription_feed(youtube)
     print(videos)
 
-def get_summary():
-    transcript = get_youtube_transcript("Pb6RYlRtEEA")
-    summary = summarize_text(transcript)
+def get_summary(video_id):
+    transcript = get_youtube_transcript(video_id)
+    summary = extended_summarize_text(transcript)
     print(summary)
 
 def run_tui():
@@ -20,5 +21,16 @@ def run_tui():
     app.run()
 
 if __name__ == "__main__":
-    logger = setup_logger()
-    run_tui()
+    parser = argparse.ArgumentParser(description="Faria")
+    
+    parser.add_argument("--tui", action="store_true", help="Run the TUI application")
+    parser.add_argument("--summarize", help="Summarize the video")
+    args = parser.parse_args()
+
+    setup_logger()
+    if args.tui:
+        run_tui()
+    elif args.summarize:
+        get_summary(args.summarize)
+    else:
+        parser.print_help()
